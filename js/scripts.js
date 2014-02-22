a443b292d6146d59889429d292f609e3417f9db7
Ali Abou-Zahr & Josh Fowler <student@epicodus.com>
2014-02-21 17:16:52 -0800

    project finalized

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,104 @@
-var letterScore = function(letter) {
-  var pts = {};
-  pts.a = pts.e = pts.i = pts.o = pts.u = pts.l = pts.n = pts.r = pts.s = pts.t = 1;
-  pts.d = pts.g = 2;
-  pts.b = pts.c = pts.m = pts.p = 3;
-  pts.f = pts.h = pts.v = pts.w = pts.y = 4;
-  pts.k = 5; 
-  pts.j = pts.x = 8;
-  pts.q = pts.z = 10;
+var numbers = function(num) {
+  var result = "";
+  var singles = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six",
+                 "7": "seven", "8": "eight", "9": "nine", "0": ""};
 
- return pts[letter];
+  var teens = {"10": "ten", "11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen", 
+               "15": "fifteen", "16": "sixteen", "17": "seventeen", "18": "eighteen", 
+               "19": "nineteen"};
+  var tens =  {"1": "ten", "2": "twenty", "3": "thirty", "4": "forty", "5": "fifty",
+               "6": "sixty", "7": "seventy", "8": "eighty", "9": "ninety", "0": ""};
 
+    if (num.length === 3) {
+      if(num[1] === "0" && num[2] === "0") {
+        result += singles[num[0]] + "-hundred";
+      } else if (num[1] === "0"){
+          result += singles[num[0]] + "-hundred-" + singles[num[2]];
+      } else if (num[2] === "0") {
+
+          result += singles[num[0]] + "-hundred-" + tens[num[1]];
+          }
+      else if (num[1] === "1") {
+        result += singles[num[0]] + "-hundred-" + teens[num.slice(1)];
+      }
+      else{
+          result += singles[num[0]] + "-hundred-" + tens[num[1]] + "-" + singles[num[2]];
+        }
+    
+    } else if (num.length === 2) {
+    if(num.charAt(0) === "1") {
+      result += teens[num]; 
+    } 
+    else if (num[1] === "0") { 
+      result += tens[num[0]]; 
+    } 
+    else {
+      result += tens[num[0]] + "-" + singles[num[1]];
+    }
+  } else {
+    result += singles[num];
+  }
+
+  return result;
 };
 
-var totalScore = function(word) {
-  var lowerCased = word.toLowerCase();
-  var splitWord = lowerCased.split("");
-  var totalScore = 0
-  for (var i = 0; i < splitWord.length; i++) {
-    totalScore += letterScore(splitWord[i]);
 
-  };
-  return totalScore;
+var bigNumbers = function(input) {
+  var scrubbedInput = input.replace(/[^0-9]/g, '');
+  if(scrubbedInput > 1000000000000) {
+    return "We said up to one-trillion, go back to school";
+  }
+  var slicedNum = scrubbedInput;
+  var numArray = [];
+  for(var i = 0; i < scrubbedInput.length / 3; i++) {
+    numArray.push(slicedNum.slice(-3))
+    slicedNum = slicedNum.slice(0, -3);
+  }
 
-};
+  var outputString = "";
+
+
+    if (numArray.length > 0){
+    outputString = numbers(numArray[0]) + outputString;
+    }
+    if (numArray.length > 1){
+    outputString = numbers(numArray[1]) + "-thousand-" + outputString;
+    }
+    if (numArray.length > 2){
+    outputString = numbers(numArray[2]) + "-million-" + outputString;
+    }
+    if (numArray.length > 3){
+    outputString = numbers(numArray[3]) + "-billion-" + outputString;
+    }
+    if (numArray.length > 4){
+      outputString = numbers(numArray[4]) + "-trillion-" + outputString;
+    }
+
+
+  return outputString;
+
+}
+
 
 $(document).ready(function() {
-  var userInput = prompt("Enter a word and recieve a scrabble score");
+  $('form').submit(function(event) {
+    var input = $("#input").val();
 
-  $('#word').text(totalScore(userInput));
+    $("#numberWord").text(bigNumbers(input));
+
+    event.preventDefault();
+  });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,32 +1,59 @@
-describe('letterScore', function() {
-
-  it('if a is entered returns 1', function() {
-    letterScore("a").should.equal(1);
+describe('numbers', function() {
+  it('should return the word one if the number 1 is entered', function() {
+    numbers("1").should.equal("one");
+  });
+  
+  it('should return the word of any number entered', function() {
+    numbers("3").should.equal("three");
   });
 
-  it('should return 1 if input is a, e, i, o, u, l, n, r, s, or t', function() {
-    letterScore("e").should.equal(1);
+  it('should return the correct word for a "teen" number', function() {
+    numbers("17").should.equal("seventeen");
   });
 
-  it('should return 2 if input is d or g', function() {
-    letterScore("g").should.equal(2);
+  it('should return the correct word for a "ten" number', function() {
+    numbers("30").should.equal("thirty");
   });
 
-  it('should return the proper score for a 1 letter word', function() {
-    letterScore("z").should.equal(10);
+  it('should return the proper word for any two digit number', function() {
+    numbers('57').should.equal("fifty-seven");
   });
 
+  it('should return the proper word for any three digit number', function(){
+    numbers('952').should.equal("nine-hundred-fifty-two");
+  });
 
+  it('should return the proper word for any three digit number', function(){
+    numbers('100').should.equal("one-hundred");
+  });
+
+  it('should return ten properly', function(){
+    numbers('10').should.equal("ten");
+  });
+
+  it('should return the proper word for a 3 digit number with 0 in the middle', function(){
+    numbers('111').should.equal("one-hundred-eleven");
+  });
+
+  it('should return the proper word for a 3 digit number with 0 at the end', function(){
+    numbers('110').should.equal("one-hundred-ten");
+  });
 });
 
-describe('totalScore', function() {
 
-  it('should return the total score of multiple letters', function() {
-    totalScore("to").should.equal(2);
+describe('bigNumbers', function() {
+
+  it('should return return the proper word value of a number in the thousands', function() {
+    bigNumbers('654321').should.equal('six-hundred-fifty-four-thousand-three-hundred-twenty-one');
   });
 
-  it('should be case insensitive', function() {
-    totalScore("Z").should.equal(10);
+  it('should return return the proper word value of a number in the millions', function() {
+    bigNumbers('987654321').should.equal('nine-hundred-eighty-seven-million-six-hundred-fifty-four-thousand-three-hundred-twenty-one');
   });
-
+  it('remove all extra characters before running', function() {
+    bigNumbers('987,654,321').should.equal('nine-hundred-eighty-seven-million-six-hundred-fifty-four-thousand-three-hundred-twenty-one');
+  });
+  it('should return properly for a number whose length is not divisable by 3', function() {
+    bigNumbers('7654321').should.equal('seven-million-six-hundred-fifty-four-thousand-three-hundred-twenty-one');
+  });
 });
